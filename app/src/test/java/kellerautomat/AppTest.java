85672d11635acac49c95d1c9fb6f9392521cc966
Manuel <manuel@strenge.ch>
2023-04-08 17:11:49 +0200

    added tests

--- app/src/test/java/kellerautomat/AppTest.java
+++ app/src/test/java/kellerautomat/AppTest.java
@@ -7,8 +7,27 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
-    @Test void appHasAGreeting() {
-        App classUnderTest = new App();
-        assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
+    @Test
+    void testInvalidOperation1() {
+        var kellerautomat = new Kellerautomat();
+        assertThrows(IllegalArgumentException.class, () -> kellerautomat.calulate("3 4 + *"));
+    }
+
+    @Test
+    void testInvalidOperation2() {
+        var kellerautomat = new Kellerautomat();
+        assertThrows(IllegalArgumentException.class, () -> kellerautomat.calulate("8 + 9 + 7 * 2 *"));
+    }
+
+    @Test
+    void testValidOperation1() {
+        var kellerautomat = new Kellerautomat();
+        assertEquals(6664,kellerautomat.calulate("3 4 + 6 2 + 8 9 + 4 3 + * * *"));
+    }
+
+    @Test
+    void testValidOperation2() {
+        var kellerautomat = new Kellerautomat();
+        assertEquals(58,kellerautomat.calulate("3 4 + 6 2 + 8 9 + 4 3 + * * *"));
     }
 }
