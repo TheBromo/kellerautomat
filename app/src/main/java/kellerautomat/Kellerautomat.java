28d170240d38f82ec1d43802f61e0640353ee3b2
Manuel <manuel@strenge.ch>
2023-04-14 10:17:18 +0200

    spellingmistake

--- app/src/main/java/kellerautomat/Kellerautomat.java
+++ app/src/main/java/kellerautomat/Kellerautomat.java
@@ -2,7 +2,7 @@ package kellerautomat;
 
 
 public class Kellerautomat {
-    public int calulate(String input) throws RuntimeException{
+    public int calculate(String input) throws RuntimeException{
         if(input.isEmpty()){
             throw new IllegalArgumentException("string is empty");
         }
--- app/src/test/java/kellerautomat/AppTest.java
+++ app/src/test/java/kellerautomat/AppTest.java
@@ -10,24 +10,24 @@ class AppTest {
     @Test
     void testInvalidOperation1() {
         var kellerautomat = new Kellerautomat();
-        assertThrows(IllegalArgumentException.class, () -> kellerautomat.calulate("3 4 + *"));
+        assertThrows(IllegalArgumentException.class, () -> kellerautomat.calculate("3 4 + *"));
     }
 
     @Test
     void testInvalidOperation2() {
         var kellerautomat = new Kellerautomat();
-        assertThrows(IllegalArgumentException.class, () -> kellerautomat.calulate("8 + 9 + 7 * 2 *"));
+        assertThrows(IllegalArgumentException.class, () -> kellerautomat.calculate("8 + 9 + 7 * 2 *"));
     }
 
     @Test
     void testValidOperation1() {
         var kellerautomat = new Kellerautomat();
-        assertEquals(6664,kellerautomat.calulate("3 4 + 6 2 + 8 9 + 4 3 + * * *"));
+        assertEquals(6664,kellerautomat.calculate("3 4 + 6 2 + 8 9 + 4 3 + * * *"));
     }
 
     @Test
     void testValidOperation2() {
         var kellerautomat = new Kellerautomat();
-        assertEquals(58,kellerautomat.calulate("3 4 + 6 2 + 8 9 + 4 3 + * * *"));
+        assertEquals(58,kellerautomat.calculate("3 4 + 6 2 + 8 9 + 4 3 + * * *"));
     }
 }
